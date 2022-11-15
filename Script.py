f5f8187f07b6d833f33e1f8dd4f432c840240171
B218088-2022 <email_not_provided>
2022-11-15 00:52:21 +0000

    commit some modification for the Script and some file generated by test running

--- Script.py
+++ Script.py
@@ -2,13 +2,52 @@
 import os
 import pandas as pd
 import subprocess
+import re
 
-def input():
-    protein = input("please input your protein")
-    taxonomy = input("please input your protein")
-
+# let use input the protein name and taxonomy group name that they want to use
+def identify():
+    protein = "_".join(input("please input your protein name\n\t").split())
+    taxonomy = "_".join(input("please input the taxonomic group you want to analyse\n\t").split())
     return protein, taxonomy
 
-os.system("esearch -db protein -query '%s[Protein Name] AND %s[Organism]' | efetch -format fasta > %s_%s.fa" % (protein, taxonmy, protein, taxonmy))
+# prepare a check function to see if the user want to continue or not
+def check_continue(information):
+    user_info = input(information + " [yes/no]\n\t")
+    if user_info.upper().startswith("Y"):
+        return True
+    else:
+        return False
+
+protein, taxonomy = identify()
+
+os.system("esearch -db protein -query '%s[Protein Name] AND %s[Organism]' | efetch -format fasta > %s_%s.fa" % (protein, taxonomy, protein, taxonomy))
+
+if __name__ == '__main__':
+    with open("%s_%s.fa" % (protein, taxonomy)) as my_file:
+        species_list = []
+        for file_line in my_file:
+            if file_line.startswith(">"):
+                organism = re.search(r"\[(.*)]", file_line).group(1)
+                species_list.append(organism)
+
+    with open("pyruvate_dehydrogenase_mammals.fa") as thefile:
+        seq = {}
+        seqlen = {}
+        for eachline in thefile:
+            if eachline.startswith(">"):
+                protein = re.search(r"\[(.*)]", eachline).group(1)
+                seq[protein] = ''
+            else:
+                seq[protein] += eachline.replace("\n", '').strip()
+            seqlen[protein] = len(seq[protein])
+            seq[protein] = seq[protein].split()
+
+
+
+
+
+
+
+
 
 
